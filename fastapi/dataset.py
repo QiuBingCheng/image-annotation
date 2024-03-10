da965e3d9a9c836187218b6fe03e60fbfdd0ed6e
QiuBingCheng <bingchengqiu@protonmail.com>
2024-03-10 14:17:22 +0800

    save mulitiplt images

---
+++ fastapi/dataset.py
@@ -0,0 +1,40 @@
+import os
+import aiofiles
+
+
+class DatasetMaker:
+
+    def __init__(self, root):
+        self.root = root
+        self.folders = ["images", "labels"]
+
+    def create_directories(self, name):
+        """
+        创建目录结构
+        """
+        path_of_dataset = os.path.join(self.root, name)
+        os.makedirs(path_of_dataset, exist_ok=True)
+
+        for folder in self.folders:
+            dir_path = os.path.join(path_of_dataset, folder)
+            os.makedirs(dir_path, exist_ok=True)
+            print(f"Created directory: {dir_path}")
+
+    async def save_images(self, name, files):
+        images_path = os.path.join(self.root, name, "images")
+
+        for no, file in enumerate(files, start=1):
+            file_name = file.filename
+            file_size = file.size/1024/1024
+
+            ext = file_name.rsplit(".", 1)[-1]
+            savefile_name = f"{no:05d}.{ext}"
+
+            print(
+                f"Received file: {file_name} ({savefile_name}), Size: {file_size} MB")
+
+            file_path = os.path.join(images_path, savefile_name)
+            print(f"Saving {file_path}")
+
+            async with aiofiles.open(file_path, "wb") as f:
+                await f.write(file.file.read())
--- fastapi/main.py
+++ fastapi/main.py
@@ -2,7 +2,18 @@ from typing import Union
 from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
+from dataset import DatasetMaker
+import asyncio
+import time
+# %%
+import configparser
+# 创建 ConfigParser 对象
+config = configparser.ConfigParser()
+# 读取配置文件
+config.read('config.ini')
+dataset_maker = DatasetMaker(config["default"]["dataset_root"])
 
+# %%
 app = FastAPI()
 
 app.add_middleware(
@@ -28,4 +39,10 @@ def read_item(item_id: int, q: Union[str, None] = None):
 async def create_upload_file(name: str = Form(...), files: List[UploadFile] = File(...)):
     print(files)
     print(name)
-    return [file.filename for file in files]
+
+    s_time = time.perf_counter()
+    dataset_maker.create_directories(name)
+    await dataset_maker.save_images(name, files)
+    e_time = time.perf_counter()
+
+    return {"status": "ok", "time": e_time-s_time}
